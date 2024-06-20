6e5655b47dc0b3f8a4df9c179417657e36f7bb79
maisiba254 <maisibajob99@gmail.com>
2024-06-21 02:10:47 +0300

    commit 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - entry point
+*
+* Description: gives output using puts function
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
+}
