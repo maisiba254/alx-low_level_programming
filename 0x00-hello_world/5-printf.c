b1c3862b0e8343ee15b7c19b82726dfb4134b28b
maisiba254 <maisibajob99@gmail.com>
2024-06-21 01:44:19 +0300

    commit 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,10 @@
+/*
+*a simple c program to display given task
+*/
 #include <stdio.h>
-int main(void)
+int main (void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return(0);
+printf ("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
 
