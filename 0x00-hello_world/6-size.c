108456f9b26c953557e2c12a2ef7cc20afda8f9d
maisiba254 <maisibajob99@gmail.com>
2024-06-21 03:47:13 +0300

    commit 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
+/**
+* main - A program to display size of various types compiled on
+* Return 0 (Success)
+*/
 int main(void)
 {
-printf("size of char: %zu byte(s)\n", sizeof(char));
-printf("size of an int: %zu Bytes\n", sizeof(int));
-printf("size of a long int: %zu bytes\n", sizeof(long int));
-rpintf("size of long long int: %zu bytes\n", sizeof(long long int));
-printf("size of float: %zu bytes\n", sizeof(float));
+char 1;
+int 2;
+long int 3;
+long long int 4;
+float 5;
+printf("size of char: %zu byte(s)\n", sizeof(1));
+printf("size of an int: %zu Bytes\n", sizeof(2));
+printf("size of a long int: %zu bytes\n", sizeof(3));
+rpintf("size of long long int: %zu bytes\n", sizeof(4));
+printf("size of float: %zu bytes\n", sizeof(5));
 return 0;
 }
