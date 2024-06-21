2fd83b26e8692681b68db35ab2b95c288b1fb52a
maisiba254 <maisibajob99@gmail.com>
2024-06-21 05:46:17 +0300

    commit 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 /**
-* main - A program to display size of various types compiled on
+* main - A program to display size of various types compiled on 32bit
+* and 64bit machines
 * Return: 0 (Success)
 */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float e;
+char a;
+int b;
+long int c;
+long long int d;
+float e;
 printf("size of a char: %zu byte(s)\n", (unsigned long) sizeof(a));
 printf("size of an int: %zu Byte(s)\n", (unsigned long) sizeof(b));
 printf("size of a long int: %zu byte(s)\n", (unsigned long) sizeof(c));
