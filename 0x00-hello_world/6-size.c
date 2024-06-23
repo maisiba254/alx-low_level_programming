c15495fd096155e99af91540bb75f04647de67dc
maisiba254 <maisibajob99@gmail.com>
2024-06-23 11:29:53 +0300

    commit 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,15 +6,10 @@
 */
 int main(void)
 {
-char a;
-int b;
-long int c;
-long long int d;
-float e;
-printf("size of a char: %lu byte(s)\n", (unsigned long) sizeof(a));
-printf("size of an int: %lu Byte(s)\n", (unsigned long) sizeof(b));
-printf("size of a long int: %lu byte(s)\n", (unsigned long) sizeof(c));
-printf("size of a long long int: %lu byte(s)\n", (unsigned long) sizeof(d));
-printf("size of a float: %lu byte(s)\n", (unsigned long) sizeof(e));
+printf("size of a char: %lu byte(s)\n", (unsigned long) sizeof(char));
+printf("size of an int: %lu Byte(s)\n", (unsigned long) sizeof(int));
+printf("size of a long int: %lu byte(s)\n", (unsigned long) sizeof(long int));
+printf("size of a long long int: %lu byte(s)\n", (unsigned long) sizeof(long long int));
+printf("size of a float: %lu byte(s)\n", (unsigned long) sizeof(float));
 return (0);
 }
