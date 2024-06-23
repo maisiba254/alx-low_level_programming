0fc472b1e46658ffb654a9b4cf23b50ad8d9c995
maisiba254 <maisibajob99@gmail.com>
2024-06-24 00:11:49 +0300

    commit 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,11 +11,11 @@ int n;
 srand(time(0));
 n = rand();
 	if (n > 0)
-	printf("%d is positive\n");
+	printf("%d is positive\n"), n;
 	else if (n < 0)
-	printf("%d is negative\n");
+	printf("%d is negative\n"), n;
 	else (n == 0)
-	printf("%d is zero");
+	printf("%d is zero"), n;
 return (0);
 }
 
