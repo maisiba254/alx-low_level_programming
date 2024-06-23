1cd07315208ca9caf1d48f2e8b00e1526aa6d24d
maisiba254 <maisibajob99@gmail.com>
2024-06-23 22:29:28 +0300

    fingers crossed

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Return: 0 (success)
+ **/
+int main(void)
+  int (n);
+{
+
+  {
+    if(n>0);
+  printf("is positive\n");
+  }
+  {
+    else(n<0);
+    printf("is negative\n");
+  }
+  {
+    elseif(n=0);
+    printf("is zero\n");
+  }
+  
+  
+}.
