c52e818a93d95822129c583649f7b4d0c0346c77
maisiba254 <maisibajob99@gmail.com>
2024-06-25 02:13:27 +0300

    commit 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Description: prints single digits of base 10
+*Return: 0 Always success
+*/
+int main(void)
+{
+int num;
+
+for (num = '0'; num <= '9'; num++)
+printf("%d", num);
+return (0);
+}
