3f412f3ae8ef16fd764c7ad986364333e1e077f6
maisiba254 <maisibajob99@gmail.com>
2024-06-25 00:49:38 +0300

    commit 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ int main(void)
 		printf("Last digit of %i is %i and is greater than 5\n", n, l);
 	else if (l < 6 && l != 0)
 		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, l);
-	else
+	else (l == 0)
 		printf("Last digit of %i is %i and is 0\n", n);
 	return (0);
 }
