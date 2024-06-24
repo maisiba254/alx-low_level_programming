fb0a9480a14c5578f018d3d49db33b9fe6ca9c42
maisiba254 <maisibajob99@gmail.com>
2024-06-25 01:15:12 +0300

    commit 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - Entry point 
+*
+*Description: alphabets in small letters
+*
+*Return: 0 Always success
+*
+*/
+int main(void)
+{
+	int ch;
+	for(ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+return (0);
+}
