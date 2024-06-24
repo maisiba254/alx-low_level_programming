bbb2136e1235f001555f18992c810d341ae6b913
maisiba254 <maisibajob99@gmail.com>
2024-06-25 01:58:53 +0300

    commit 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Description: eliminates letters q and e from the alphabet
+*Return: 0 Always success
+*/
+int main(void)
+{
+int ch;
+
+for (ch = 'a'; ch ='z'; ch++)
+	{if (ch !='q'; ch !='e')}
+putchar(ch);
+return (0);
+}
