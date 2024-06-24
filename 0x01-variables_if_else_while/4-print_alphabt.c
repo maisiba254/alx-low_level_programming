c903bda3f7e668e1d8b58f43f1cadbf15b132003
maisiba254 <maisibajob99@gmail.com>
2024-06-25 02:34:08 +0300

    commit 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,7 +9,11 @@ int main(void)
 int ch;
 
 for (ch = 'a'; ch ='z'; ch++)
-	{if (ch !='q' && ch !='e')
-putchar(ch);}
+	{
+	if (ch !='q' && ch !='e')
+	putchar(ch);
+}
+putchar('\n');
+
 return (0);
 }
