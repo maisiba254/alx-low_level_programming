d2b827961ca021a8f3e27b963a5fa32e7fdb5064
maisiba254 <maisibajob99@gmail.com>
2024-06-25 00:42:31 +0300

    commit 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+
 /*main - Entry point
 * Return - 0 always success
-/* betty style doc for function main goes there */
+*/
 int main(void)
 {
 	int n;
-
+	int l;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
