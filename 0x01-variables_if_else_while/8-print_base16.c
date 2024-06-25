22a955bda15f57c51379098b7d921ae7bbaff39d
maisiba254 <maisibajob99@gmail.com>
2024-06-25 04:42:21 +0300

    commit 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: 0 Always success
+*/
+int main(void)
+{
+int ch;
+int num;
+
+for (num = '0'; num <= '9'; num++)
+putchar(num);
+for (ch = 'a'; ch <= 'f'; ch++
+putchar(ch);
+putchar('\n');
+return (0);
+}
