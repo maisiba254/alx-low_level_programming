d1b0d536229e515f7b3800841c4862309c8fee26
maisiba254 <maisibajob99@gmail.com>
2024-06-25 05:02:39 +0300

    commit 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: 0 Always success
+*/
+int main(void)
+{
+int num;
+
+for (num = '0'; num <= '9'; num++)
+putchar('0' + num);
+{
+if (num != 9)
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
