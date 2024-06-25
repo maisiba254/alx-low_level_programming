506ea3d0f2a4b0f541bfca01a31f6c31e72b7e1c
maisiba254 <maisibajob99@gmail.com>
2024-06-25 04:04:22 +0300

    commit 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*main - Entry point 
+*Return: 0 Always success
+*/
+int main (void)
+{
+int ch;
+for (ch = 'z'; ch >= 'a'; ch--)
+putchar(ch);
+putchar('\n');
+return (0);
+}
