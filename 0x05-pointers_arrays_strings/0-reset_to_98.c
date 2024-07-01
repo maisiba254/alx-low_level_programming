49345c070e3424f47530d4ab70213de0b9a6beb8
maisiba254 <maisibajob99@gmail.com>
2024-07-02 01:15:31 +0300

    commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
-* main - update value it points to  to 98
+*main - update value it points to  to 98
 *reset_to_98 - resets value of an int to 98
 *@n: pointer to an int
 *Return: 0
 */
 
-void reset_to_98(int *n)
+void reset_to_98(int *n)/* esets value of an int to 98 */
 {
 	*n = 98;
 }
